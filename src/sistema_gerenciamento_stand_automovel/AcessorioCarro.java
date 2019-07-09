f59d425f607e38e43f084635bd6fde6fc8f0dfa1
Megamente14 <52715286+Megamente14@users.noreply.github.com>
2019-07-09 20:17:41 +0000

    Stands

---
+++ src/sistema_gerenciamento_stand_automovel/AcessorioCarro.java
@@ -0,0 +1,34 @@
+
+package sistema_gerenciamento_stand_automovel;
+
+
+public class AcessorioCarro {
+    
+    private   boolean air_bag;
+    private boolean Computador_digital;
+    private boolean ar_condionado;
+
+    public boolean isAir_bag() {
+        return air_bag;
+    }
+
+    public void setAir_bag(boolean air_bag) {
+        this.air_bag = air_bag;
+    }
+
+    public boolean isComputador_digital() {
+        return Computador_digital;
+    }
+
+    public void setComputador_digital(boolean Computador_digital) {
+        this.Computador_digital = Computador_digital;
+    }
+
+    public boolean isAr_condionado() {
+        return ar_condionado;
+    }
+
+    public void setAr_condionado(boolean ar_condionado) {
+        this.ar_condionado = ar_condionado;
+    }
+}
